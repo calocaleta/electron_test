9d0e11db14d0cca6d5c23f15d7f76593466bc69c
Carlos A. Garcia Gonzales <cgarcia@stsac.com>
2022-10-17 18:59:42 -0500

    Including electron packager

--- index.js
+++ index.js
@@ -9,10 +9,28 @@ app.on('before-quit', () => {
 })
 
 app.on('ready', ()=>{
-    let win = new BrowserWindow()
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'Aplicativo Electron',
+        center: true,
+        //maximizable: false,
+        show: false
+    })
+    
+    win.once('ready-to-show', () => {
+        win.show()
+    })
+
+    win.on('move',()=>{
+        const position = win.getPosition()
+        //console.log(`la posición de la ventana es ${position}`)
+    })
 
     win.on('closed',()=>{
         win = null
         app.quit()
     })
+
+    win.loadURL('http://devdocs.io')
 })
